eead6f7ab92867f59db2d2d9174050ec35e158cf
Jefferson Rylee <rylee.jeff385@gmail.com>
2025-04-21 18:59:02 +0800

    Update rollup config: remove import workaround and update size plugin

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,15 +1,11 @@
-import _eslint from '@rollup/plugin-eslint';
-import _typescript from '@rollup/plugin-typescript';
+import eslint from '@rollup/plugin-eslint';
+import typescript from '@rollup/plugin-typescript';
 import { OutputOptions, RollupOptions } from 'rollup';
 import cleanup from 'rollup-plugin-cleanup';
 import dts from 'rollup-plugin-dts';
 import esbuild, { Options as EsbuildOptions } from 'rollup-plugin-esbuild';
 import outputSize from 'rollup-plugin-output-size';
 import pkg from './package.json' with { type: 'json' };
-
-// NOTE: remove once import errors are fixed for their respective packages
-const eslint = _eslint as unknown as typeof _eslint.default;
-const typescript = _typescript as unknown as typeof _typescript.default;
 
 // skip sourcemap and umd unless production
 const PROD = process.env.NODE_ENV !== 'development';
@@ -27,6 +23,10 @@ function clean() {
     comments: ['some', 'sources', /__PURE__/],
     extensions: ['js', 'ts']
   });
+}
+
+function size() {
+  return outputSize({ bytes: true });
 }
 
 function umd(options: Partial<OutputOptions>): OutputOptions {
@@ -50,22 +50,22 @@ export default defineConfig([
       { file: pkg.main, format: 'cjs', exports: 'named', sourcemap: PROD },
       { file: pkg.module, format: 'esm', exports: 'named', sourcemap: PROD }
     ],
-    plugins: [build(), clean(), outputSize()]
+    plugins: [build(), clean(), size()]
   },
   PROD && {
     input: inputUmd,
     output: umd({ file: pkg.unpkg.replace(/\.min\.js$/, '.js') }),
-    plugins: [build(), clean(), outputSize()]
+    plugins: [build(), clean(), size()]
   },
   PROD && {
     input: inputUmd,
     output: umd({ file: pkg.unpkg }),
-    plugins: [build({ minify: true }), clean(), outputSize()]
+    plugins: [build({ minify: true }), clean(), size()]
   },
   {
     input,
     output: { file: pkg.types, format: 'esm' },
-    plugins: [dts(), outputSize()]
+    plugins: [dts(), size()]
   },
   WATCH && {
     input,
